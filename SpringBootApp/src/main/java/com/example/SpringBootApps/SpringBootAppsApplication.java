cad4ba5c5c5ebddfba81484a2feb05e22c3554e9
punya <punya@DESKTOP-IBKE6NU>
2020-07-18 21:32:04 +0530

    syso message added

--- SpringBootApp/src/main/java/com/example/SpringBootApps/SpringBootAppsApplication.java
+++ SpringBootApp/src/main/java/com/example/SpringBootApps/SpringBootAppsApplication.java
@@ -20,6 +20,7 @@ public class SpringBootAppsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAppsApplication.class, args);
+		System.out.println("hello");
 	}
 
 }
